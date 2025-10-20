9669d9a8a693f7759df2a0209849da0ca351520d
CODE-AXION <codeaxion77@gmail.com>
2025-10-20 16:17:30 +0530

    Update BloodRequest model to include rejection_reason field and modify related database queries

---
+++ app/Filament/Resources/BloodRequestResource.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\BloodRequestResource\Pages;
+use App\Filament\Resources\BloodRequestResource\RelationManagers;
+use App\Models\BloodRequest;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class BloodRequestResource extends Resource
+{
+    protected static ?string $model = BloodRequest::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                //
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                //
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListBloodRequests::route('/'),
+            'create' => Pages\CreateBloodRequest::route('/create'),
+            'edit' => Pages\EditBloodRequest::route('/{record}/edit'),
+        ];
+    }
+}
